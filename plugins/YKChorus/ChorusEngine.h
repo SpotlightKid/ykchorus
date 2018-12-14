5847c2a0926a3f7afe02a183606043f335ba06a7
Christopher Arndt <chris@chrisarndt.de>
2018-12-14 16:46:06 +0100

    Code formatting: replace all tabs with spaces
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- plugins/YKChorus/ChorusEngine.h
+++ plugins/YKChorus/ChorusEngine.h
@@ -1,24 +1,24 @@
 /*
-	==============================================================================
-	This file is part of Tal-NoiseMaker by Patrick Kunz.
+    ==============================================================================
+    This file is part of Tal-NoiseMaker by Patrick Kunz.
 
-	Copyright(c) 2005-2010 Patrick Kunz, TAL
-	Togu Audio Line, Inc.
-	http://kunz.corrupt.ch
+    Copyright(c) 2005-2010 Patrick Kunz, TAL
+    Togu Audio Line, Inc.
+    http://kunz.corrupt.ch
 
-	This file may be licensed under the terms of of the
-	GNU General Public License Version 2 (the ``GPL'').
+    This file may be licensed under the terms of of the
+    GNU General Public License Version 2 (the ``GPL'').
 
-	Software distributed under the License is distributed
-	on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
-	express or implied. See the GPL for the specific language
-	governing rights and limitations.
+    Software distributed under the License is distributed
+    on an ``AS IS'' basis, WITHOUT WARRANTY OF ANY KIND, either
+    express or implied. See the GPL for the specific language
+    governing rights and limitations.
 
-	You should have received a copy of the GPL along with this
-	program. If not, go to http://www.gnu.org/licenses/gpl.html
-	or write to the Free Software Foundation, Inc.,
-	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-	==============================================================================
+    You should have received a copy of the GPL along with this
+    program. If not, go to http://www.gnu.org/licenses/gpl.html
+    or write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+    ==============================================================================
  */
 
 #if !defined(__ChorusEngine_h)
